81f1ecc98a43a1e64fec231a96576ddd67106f52
Sduncanpdx <62905755+Sduncanpdx@users.noreply.github.com>
2020-06-02 16:33:15 -0700

    k-means with sum squared error

--- kmeans.py
+++ kmeans.py
@@ -6,7 +6,7 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 #Regression program for k-means and fuzzy c-means
 #by spencer Duncan
 
-clusters = 10
+clusters = 15
 
 
 def readdata(filename):
@@ -21,22 +21,44 @@ def readdata(filename):
     return data
 
 
-
+#update clusters
 def e(w, data, centroids):
-    
+    for i in range(data.shape[0]):
+        norms = np.linalg.norm(centroids-data[i], axis=1)
+        w[i] = np.argmin(norms, axis=0)
+    print("labels:",w)
     return
 
+#calc new centroids
 def m(w, data, centroids):
-    for i in range(10):
+    global clusters
+    clusterRange = clusters
+    for i in range(clusterRange):
         pred = w[:] == i
         cluster = data[pred]
-        centroids[i] = np.mean(cluster, axis=0)
-        print(centroids)
-    pl.scatter(centroids[:,0], centroids[:,1],)
-    pl.show()
+        if(cluster.shape[0] == 0):
+            np.delete(centroids, i)
+            clusters = clusters-1
+        else:
+            centroids[i] = np.mean(cluster, axis=0)
     return
 
-    
+def display(w, data, centroids):
+    colors = w/clusters
+    pl.scatter(data[:,0], data[:,1],c=colors)
+    pl.scatter(centroids[:,0], centroids[:,1], c='r')
+    pl.show()
+
+def calcError(w, data, centroids):
+    sum = 0
+    for i in range(clusters):
+        pred = w[:] == i
+        cluster = data[pred]
+        diff = np.linalg.norm(cluster-centroids[i], axis=1)
+        diff = np.square(diff)
+        diff = np.sum(diff)
+        sum += diff
+    return sum
 
 def main():
     filename = "cluster_dataset.txt"
@@ -45,15 +67,26 @@ def main():
     datasize = data.shape[0]
     #used for fuzzy c means
     #w = np.random.rand(datasize, clusters)
+    absCord = max(abs(np.min(data[:,0])),abs(np.max(data[:,0])),abs(np.min(data[:,1])),abs(np.max(data[:,1])))
+    print("abs:",absCord)
+    centroids = np.random.uniform(low=-absCord, high=absCord, size=(clusters,2))
+    print("centroids:",centroids)
     w = np.random.randint(clusters,size=datasize)
-    centroids = np.zeros((clusters,2))
-    m(w, data, centroids)
+    e(w, data, centroids)
     print(w)
-    error = 1
-    while(error > .2):
+    pError = 0
+    error = 0
+    deltError = 100
+    while(deltError > 1):
+        m(w, data, centroids)
+        e(w, data, centroids)
+        error = calcError(w, data, centroids)
+        print(error)
+        display(w, data, centroids)
+        deltError = abs(pError-error)
+        pError = error
         
         
-        error -= .2
 
 
 main()
